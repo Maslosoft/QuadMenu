f159e4c1212ad2d0d938854cebc920ac558424a2
Peter Maselkowski <pmaselkowski@gmail.com>
2015-09-10 16:46:17 +0200

    +~ Composer/php draft

---
+++ src/QuadMenu.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Maslosoft\QuadMenu;
+
+/**
+ * QuadMenu
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class QuadMenu
+{
+
+	use \Maslosoft\EmbeDi\Traits\FlyTrait;
+}
